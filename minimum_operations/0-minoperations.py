3fdc32a8ef8f9f111c4eef9fc1a6ecbf89677883
shakilla1 <s.uwamahoro1@alustudent.com>
2026-01-14 06:05:39 -0800

    add my file

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Minimum Operations project"""
+
+
+def minOperations(n):
+    
+    if n <= 1:
+        return 0
+    i = 2
+    result = 0
+    while i <= n:
+        if n % i == 0:
+            result += i
+            n = n / i
+        else:
+            i += 1
+    return result
