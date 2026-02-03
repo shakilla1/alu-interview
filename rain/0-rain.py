34020107849e3ea8f89f2b69abbbcedad10579c9
shakilla1 <s.uwamahoro1@alustudent.com>
2026-02-03 05:11:48 -0800

    add everything

---
+++ rain/0-rain.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def rain(walls):
+    if not walls:
+        return 0
+
+    left = 0
+    right = len(walls) - 1
+    left_max = 0
+    right_max = 0
+    water = 0
+
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
+
+    return water
